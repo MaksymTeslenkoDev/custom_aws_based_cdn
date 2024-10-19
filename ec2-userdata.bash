1207adbd6571640d6129376aa261db6f17181ae5
maksim.t <maksim.teslenko.dev@gmail.com>
2024-10-19 17:53:02 +0300

    fix ec2 user data script config

--- ec2-userdata.bash
+++ ec2-userdata.bash
@@ -13,4 +13,4 @@ sudo systemctl enable docker
 # Pull your Docker image from Docker Hub
 sudo docker pull devmaksdev/nginx-reverse-proxy-cache:amd64
 
-docker run -d -p 80:80 --name nginx-cache-server -e S3_BUCKET_NAME="www.my.githubresumebuilder.cloud.s3-website-us-east-1.amazonaws.com" devmaksdev/nginx-cache-purge:latest
+docker run -d -p 80:80 --name nginx-cache-server -e S3_BUCKET_NAME="www.my.githubresumebuilder.cloud.s3-website-us-east-1.amazonaws.com" devmaksdev/nginx-reverse-proxy-cache:amd64
