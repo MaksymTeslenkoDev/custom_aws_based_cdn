ea1558b84de4045ed40fe8b3e3c5e9e70db225ea
maksim.t <maksim.teslenko.dev@gmail.com>
2024-10-21 19:45:50 +0300

    update docker compose, update userdata script for ec2 cdn.lb instance

--- ec2-userdata.cdn.balancer.bash
+++ ec2-userdata.cdn.balancer.bash
@@ -1,17 +1,63 @@
 #!/bin/bash
-
-# Update package lists
-sudo apt-get update
+# Update the instance
+sudo apt-get update -y
 
 # Install Docker
 sudo apt-get install -y docker.io
 
-# Ensure Docker starts on boot
-sudo systemctl start docker
+# Enable Docker service to start on boot
 sudo systemctl enable docker
+sudo systemctl start docker
 
-# Pull your Docker image from Docker Hub
-sudo docker pull devmaksdev/nginx-reverse-proxy-cache:amd64
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-# verify that provided public ip's point to existed reverse-cache-proxy servers
-docker run -d -p 80:80 --name cdn-lb -e CACHE_SERVERS="server 13.51.201.229; server 35.180.16.210;" devmaksdev/cdn-least-con-lb-image:latest
+# Verify Docker Compose installation
+docker-compose --version
+
+# Create a directory for your Docker setup
+mkdir -p /home/ubuntu/docker-setup
+cd /home/ubuntu/docker-setup
+
+# Create the docker-compose.yml file
+cat <<EOF > /home/ubuntu/docker-setup/docker-compose.yml
+version: '3'
+services:
+  cache-proxy-1:
+    image: devmaksdev/nginx-reverse-proxy-cache:amd64
+    container_name: cache-proxy-1
+    environment:
+      S3_BUCKET_NAME: www.my.githubresumebuilder.cloud.s3-website-us-east-1.amazonaws.com
+    ports:
+      - "8081:80"
+    restart: always
+
+  cache-proxy-2:
+    image: devmaksdev/nginx-reverse-proxy-cache:amd64
+    container_name: cache-proxy-2
+    environment:
+      S3_BUCKET_NAME: www.my.githubresumebuilder.cloud.s3-website-us-east-1.amazonaws.com
+    ports:
+      - "8082:80"
+    restart: always
+  
+  cdn-lb:
+    image: devmaksdev/cdn-least-con-lb-image:latest
+    container_name: cdn-lb
+    environment:
+      - CACHE_SERVERS=server cache-proxy-1; server cache-proxy-2;
+    ports:
+      - "80:80"
+    depends_on:
+      - cache-proxy-1
+      - cache-proxy-2
+    restart: always
+EOF
+
+# Change directory permissions
+sudo chown -R ubuntu:ubuntu /home/ubuntu/docker-setup
+
+# Run docker-compose to pull images and start the services
+cd /home/ubuntu/docker-setup
+sudo docker-compose up -d
